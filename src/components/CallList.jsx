2db2c25deba66227a093236f110154f3e4727705
Nikhil Singh <youremail@domain.com>
2024-01-03 20:26:57 +0530

    Api call

--- src/components/CallList.jsx
+++ src/components/CallList.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
-import ArchivedCall from './ArchivedCall';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { baseUrl, endPoint } from '../config/url';
 
-const PhoneDirectory = () => {
+
+const CallList = () => {
   const [archivedCallList, setArchivedCallList] = useState([])
   const [checkArchived, setCheckArchived] = useState(false)
-
   const [callList, setCallList] = useState([
     { id: 1, name: 'John Doe', phoneNumber: '9149323087' },
     { id: 2, name: 'Jane Smith', phoneNumber: '9149323087' },
@@ -13,6 +14,24 @@ const PhoneDirectory = () => {
     { id: 5, name: 'Alice Johnson', phoneNumber: '9149323087' },
     { id: 6, name: 'Alice Johnson', phoneNumber: '9149323087' },
   ]);
+  const baseurl = baseUrl.baseUrl
+  const route = endPoint.activities;
+
+
+  const fetchActivitiesData =  async () => {
+    try {
+      const response = await axios.get(baseurl + route);
+      console.log(response, '...............response')
+      // setData(response.data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }
+
+  useEffect(() => {
+    fetchActivitiesData();
+  }, []);
+
 
   const handleArchivedCalls = (index) => {
     let newCallList = callList?.filter(call => call?.id != index)
@@ -20,28 +39,20 @@ const PhoneDirectory = () => {
     setCallList(newCallList)
   }
 
-  
+  console.log(callList, 'Call')
 
   return (
     <div>
-      <div onClick={() => setCheckArchived(true)}>Archived</div>
-      {checkArchived ?
-        <ArchivedCall callList={archivedCallList} /> :
-
-
-        <>
-          <h2>AirCall Phone</h2>
-          <ul>
-            {callList.map((contact) => (
-              <li key={contact?.id}>
-                {contact.name}: {contact.phoneNumber} : <button onChange={handleArchivedCalls(contact?.id)}>X</button>
-              </li>
-            ))}
-          </ul>
-        </>
-      }
+      <h2>AirCall Phone</h2>
+      <ul>
+        {callList.map((contact) => (
+          <li key={contact?.id}>
+            {contact.name}: {contact.phoneNumber}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
 
-export default PhoneDirectory;
+export default CallList;
